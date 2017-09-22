2d4875caa456ff4ee5dbf4c7f9f6196e3ed104fc
usuario <usuario@10.22.148.62>
2017-09-22 09:36:59 +0100

    Kata1: Calculo de la edad de una persona usando la clase obsoleta Date

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,27 +1,35 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 public class Person {
     private final String Name;
-    private final Date birthdate;
-
+    private final Calendar birthdate;
+    private final long millisecond_Per_Year =
+                    (long)(1000*60*60*24*365.25);
     public String getName() {
         return Name;
     }
 
-    public Date getBirthdate() {
+    public Calendar getBirthdate() {
         return birthdate;
     }
 
     
 
-    public Person(String Name, Date birthdate) {
+    public Person(String Name, Calendar birthdate) {
         this.Name = Name;
         this.birthdate = birthdate;
     }
     public int getAge(){
-        return (int) ((new Date().getTime() - 
-                birthdate.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int)(millisecondsToYear(today.getTimeInMillis()
+                       - birthdate.getTimeInMillis()));
+    }
+    private long millisecondsToYear(long num){
+        return num / millisecond_Per_Year;
+        
     }
 }
